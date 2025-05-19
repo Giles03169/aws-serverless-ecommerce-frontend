eca6120a421aaa19b8d10a79c0aa5ed95fb2f89d
Giles03169 <giles.woodford@icloud.com>
2025-05-19 10:34:23 +0200

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,13 @@
+import React from 'react'
+import ProductList from './components/ProductList'
 
+function App() {
+  return (
+    <div>
+      <h1>🛍️ My Serverless eCommerce Store</h1>
+      <ProductList />
+    </div>
+  )
+}
+
+export default App
